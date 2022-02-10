be1d58538c4849ee2c8340c0de20cf8ff1d2cfc5
Tung-gh <tung.uet.work@gmail.com>
2022-02-10 18:17:14 +0700

    create Modules.Model python file

---
+++ Modules/Model.py
@@ -0,0 +1,34 @@
+class Model:
+    def train(selfself, inputs, outputs):
+        """
+
+        :param inputs:
+        :param outputs:
+        :return:
+        """
+        raise NotImplementedError
+
+    def save(self, path):
+        """
+
+        :param path:
+        :return:
+        """
+        raise NotImplementedError
+
+    def load(self, path):
+        """
+
+        :param path:
+        :return:
+        """
+        raise NotImplementedError
+
+    def predict(self, inputs):
+        """
+
+        :param inputs:
+        :return:
+        :rtype: list of models.Output
+        """
+        raise NotImplementedError
