bc20010d0ee7c3b4dfaff5d1f9c2c7e1176be2f7
Tung-gh <tung.uet.work@gmail.com>
2022-02-15 01:01:59 +0700

    Fix preprocess bugs

--- Modules/Preprocess.py
+++ Modules/Preprocess.py
@@ -1,5 +1,5 @@
 import sys
-
+import re
 import pandas as pd
 import string
 
@@ -9,14 +9,17 @@ from Input_Output import Input, Output
 punctuations = list(string.punctuation)
 useless_labels = ['295', '296', '314', '315', '329', '330', '348', '349']
 
-def isNan(string):
-    return string != string
 
-def contains_punctuation(string):
-    for c in string:
+def is_nan(str):
+    return str != str
+
+
+def contains_punctuation(str):
+    for c in str:
         if c in punctuations:
             return True
     return False
+
 
 def contains_digit(w):
     for i in w:
@@ -24,11 +27,13 @@ def contains_digit(w):
             return True
     return False
 
+
 def typo_trash_labeled(lst):
     for i in lst:
         if i in useless_labels:
             return True
     return False
+
 
 def load_data(data_path, num_aspects):
     if num_aspects == 6:
@@ -37,35 +42,44 @@ def load_data(data_path, num_aspects):
         categories = ['cấu hình','mẫu mã','hiệu năng','ship','giá','chính hãng','dịch vụ','phụ kiện']
 
     inputs, outputs = [], []
-    df = pd.read_csv(data_path, encoding='utf-8')
+    df = pd.read_csv(data_path,  encoding='utf-8')
     aspects = list(range(num_aspects))
     for index, row in df.iterrows():
         annotation = row['annotations']
         _annotation = annotation.strip().split(', ')
-        input_annotation = [str(_annotation[i][-3:]) for i in range(0, len(_annotation), 2)]
+        text_annotation = [str(_annotation[i][-3:]) for i in range(0, len(_annotation), 2)]
 
-        if isNan(row['text']) == 0 and typo_trash_labeled(input_annotation) == 0:
+        if is_nan(row['text']) == 0 and typo_trash_labeled(text_annotation) == 0:
             text = row['text'].strip()
-            inputs.append(Input(text, input_annotation))
+            inputs.append(Input(text, text_annotation))
 
             scores = [0 if row[categories[i]] == 0 else 1 for i in range(0, num_aspects)]
             outputs.append(Output(aspects, scores))
 
     return inputs, outputs
 
+
+def make_corpus(inputs):
+    corpus = []
+    for ip in inputs:
+        corpus.append(ip.text)
+
+    return corpus
+
+
 def preprocess_inputs(inputs):
     for ip in inputs:
-        text = ip.text.strip()
-        _text = text.split(' ')
-        for token in _text:
-            if contains_punctuation(token) == 0:
-                _text.remove(token)
-        for i in range(len(_text)):
-            if contains_digit(_text[i]):
-                _text[i] = '0'
-        ip.text = ' '.join(_text)
+        text = ip.text.strip().replace('_', ' ').split(' ')
+        for j in range(len(text)):
+            if contains_digit(text[j]):
+                text[j] = '0'
+        for token in text:
+            if len(token) <= 1 or token in punctuations:
+                text.remove(token)
+        ip.text = ' '.join(text)
 
     return inputs
+
 
 def make_vocab(inputs):
     vocab = []
@@ -73,10 +87,10 @@ def make_vocab(inputs):
         text = ip.text.split(' ')
         for token in text:
             vocab.append(token)
-    #Make a non-duplicated vocabulary
+    # Make a non-duplicated vocabulary
     vocab = list(dict.fromkeys(vocab))
 
-    with open(r"H:/DS&KT Lab/NCKH/OpinionMining/data_mebe/" + str(sys.argv[1]) + "_vocab.txt", 'w', encoding='utf8') as f:
+    with open(r"H:/DS&KT Lab/NCKH/Aquaman/data/data_mebe/" + str(sys.argv[1]) + "_vocab.txt", 'w', encoding='utf8') as f:
         for w in vocab:
             f.write('{}\n'.format(w))
 
--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import sys
 
-from Modules.Preprocess import load_data, preprocess_inputs, make_vocab
+from Modules.Preprocess import load_data, make_corpus, preprocess_inputs, make_vocab
 
 datasets = {'mebeshopee': [6, 0],
               'mebetiki': [6, 1],
               'techshopee': [8, 2],
               'techtiki': [8, 3] }
 data_paths = [
-    r"H:\DS&KT Lab\NCKH\OpinionMining\data_mebe\mebeshopee.csv",
-    r"H:\DS&KT Lab\NCKH\OpinionMining\data_mebe\mebetiki.csv"
+    r"H:\DS&KT Lab\NCKH\Aquaman\data\data_mebe\mebeshopee.csv",
+    r"H:\DS&KT Lab\NCKH\Aquaman\data\data_mebe\mebetiki.csv"
 ]
 
 
 if __name__ == '__main__':
-    set = sys.argv[1]
-    if set not in list(datasets.keys()):
-        print('There is no category', set)
+    argv = sys.argv[1]
+    if argv not in list(datasets.keys()):
+        print('There is no category', argv)
         exit
     else:
-        data_path = data_paths[datasets[set][1]]
-        num_aspects = datasets[set][0]
-        #Load inputs, outputs from data file
-        inputs, outputs = load_data(data_path, num_aspects)     #2086 samples, 2086 samples
-        #Preprocess the inputs data
+        data_path = data_paths[datasets[argv][1]]
+        num_aspects = datasets[argv][0]
+        # Load inputs, outputs from data file
+        inputs, outputs = load_data(data_path, num_aspects)     # 2086 samples, 2086 samples
+        # Preprocess the inputs data
         inputs = preprocess_inputs(inputs)
-        #Make a vocabulary from the inputs data
-        vocab = make_vocab(inputs)                              #1138 words
+        # Make a vocabulary from the inputs data
+        vocab = make_vocab(inputs)                              #1354 words
+        print(len(vocab))
 
-        print(inputs[0].text)
 
